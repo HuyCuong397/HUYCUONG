b084a5dfdbc739dc3bcf5f66440441f5665e0242
HuyCuong397 <tranhuycuong310507@gmail.com>
2025-03-26 10:08:33 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,10 @@
-
+function login()
+{
+  let u = username.value
+  let p = password.value
+  if (u=="cuong" && p=="123")
+  {
+    window.open("home.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
